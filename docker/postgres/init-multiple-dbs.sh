fc63c5489e85546dcff65c423c7cb00a6186bc71
Jsua3 <jjsua_542@unihumboldt.edu.co>
2026-04-22 13:39:05 -0500

    BIG CHANGES

--- docker/postgres/init-multiple-dbs.sh
+++ docker/postgres/init-multiple-dbs.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 set -u
 
-function create_user_and_database() {
+create_user_and_database() {
 	local database=$1
 	echo "  Creating database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
